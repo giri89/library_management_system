4a2c69e1c623e20eec496498d51c4df4ab9abec8
Girijesh Kumar Rajak <girijeshkrrajak@gmail.com>
2026-06-01 23:20:39 +0530

    Add IssueRecord model class for issue tracking

--- src/com/librarymanagement/model/IssueRecord.java
+++ src/com/librarymanagement/model/IssueRecord.java
@@ -7,39 +7,25 @@ public class IssueRecord {
     private int bookId;
     private int memberId;
     private LocalDate issueDate;
+    private LocalDate dueDate;
     private LocalDate returnDate;
     private String status;
 
-    public IssueRecord(int issueId, int bookId, int memberId, LocalDate issueDate, LocalDate returnDate, String status) {
+    public IssueRecord(int issueId, int bookId, int memberId, LocalDate issueDate, LocalDate dueDate, LocalDate returnDate, String status) {
         this.issueId = issueId;
         this.bookId = bookId;
         this.memberId = memberId;
         this.issueDate = issueDate;
+        this.dueDate = dueDate;
         this.returnDate = returnDate;
         this.status = status;
     }
 
-    public int getIssueId() {
-        return issueId;
-    }
-
-    public int getBookId() {
-        return bookId;
-    }
-
-    public int getMemberId() {
-        return memberId;
-    }
-
-    public LocalDate getIssueDate() {
-        return issueDate;
-    }
-
-    public LocalDate getReturnDate() {
-        return returnDate;
-    }
-
-    public String getStatus() {
-        return status;
-    }
+    public int getIssueId()           { return issueId; }
+    public int getBookId()            { return bookId; }
+    public int getMemberId()          { return memberId; }
+    public LocalDate getIssueDate()   { return issueDate; }
+    public LocalDate getDueDate()     { return dueDate; }
+    public LocalDate getReturnDate()  { return returnDate; }
+    public String getStatus()         { return status; }
 }
